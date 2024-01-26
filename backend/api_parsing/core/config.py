0742ab5888c76ea159624edd20e5812b581a6b87
kochenov <kochenof@yandex.ru>
2024-01-26 13:55:21 +0300

    Add pydantic_settings and config.py

---
+++ backend/api_parsing/core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    api_prefix: str
+    db_host: str
+    db_port: int
+    db_name: str
+    db_user: str
+    db_pass: str
+    secret: str
+    secret_parse: int
+    log_level: str
+
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
