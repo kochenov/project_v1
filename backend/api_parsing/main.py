56496e616810ff65b2c5a0b3856a8d8a5874472c
kochenov <kochenof@yandex.ru>
2024-01-26 10:00:19 +0300

    Add uvicorn

--- backend/api_parsing/main.py
+++ backend/api_parsing/main.py
@@ -1,4 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
 
-if __name__ == '__main__':
-    pass
+app = FastAPI(
+    title="DreamManor",
+    version="0.0.1",
+)
 
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello index!",
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", log_level="info", reload=True)
