40b5e6b74c5f9b2c7f632f014b8d47051335685b
kochenov <kochenof@yandex.ru>
2024-02-19 14:30:49 +0300

    Fix error app run

--- backend/api_parsing/main.py
+++ backend/api_parsing/main.py
@@ -27,4 +27,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main.app:app", host="127.0.0.1", log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", log_level="info", reload=True)
