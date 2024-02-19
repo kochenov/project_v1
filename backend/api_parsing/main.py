e6575fd6c8b88e7b344ba8a598d9788d86d2e035
kochenov <kochenof@yandex.ru>
2024-02-19 14:21:49 +0300

    Codebase to Start

--- backend/api_parsing/main.py
+++ backend/api_parsing/main.py
@@ -1,18 +1,30 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+# инициализация приложения
 app = FastAPI(
-    title="DreamManor",
+    title="DreamManor Parser Avito",
     version="0.0.1",
 )
 
+origins = [
+    "http://localhost:9000",
+]
 
-@app.get("/")
-def hello_index():
-    return {
-        "message": "Hello index!",
-    }
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", log_level="info", reload=True)
+    uvicorn.run("main.app:app", host="127.0.0.1", log_level="info", reload=True)
